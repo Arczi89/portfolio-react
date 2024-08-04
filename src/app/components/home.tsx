a7bff29105099d4b4f703aebad12091501140f2a
Artur Szwagrzak <artur@szwagrzak.pl>
2024-08-04 15:37:34 +0200

    add linkedin icon to contact section

--- src/app/components/home.tsx
+++ src/app/components/home.tsx
@@ -1,5 +1,7 @@
 'use client';
+import Link from 'next/link';
 import React from 'react';
+import { FaLinkedin } from "react-icons/fa6";
 
 const Home = () => {
   return (
@@ -13,9 +15,11 @@ const Home = () => {
       <main className="container mx-auto py-6 flex-grow bg">
         <section className="mb-8">
           <h2 className="text-2xl font-bold">O mnie</h2>
-          <p className="mt-2">
-            Jestem entuzjastą technologii i pasjonatem pisania kodu. Moje zainteresowania obejmują frontend, backend oraz wszystko, co związane z aplikacjami internetowymi.
-          </p>
+          <p className="mt-2"></p>
+          <p>Jestem programistą z ponad 10-letnim doświadczeniem, obejmującym pisanie aplikacji internetowych (Java Struts, Angular), aplikacji na system Android (Kotlin) oraz pracę na backendzie (Java, SQL). W mojej karierze zajmowałem się tworzeniem i naprawianiem różnorodnych funkcjonalności, a także pisałem testy, korzystając z nowoczesnych frameworków.</p>
+          <p>Pracowałem zarówno w zespołach SCRUMowych, jak i międzynarodowych, gdzie konieczna była ciągła komunikacja i współpraca z klientami zagranicznymi w języku angielskim. Moje główne zainteresowania skupiają się na technologiach webowych, JavaScript, zwłaszcza na frameworku Angular. Dodatkowo, pasjonuję się tematyką testów jednostkowych oraz podejścia TDD. Zawsze stawiam na wysoką jakość kodu, przestrzegając zasad clean code.</p>
+          <p>Posiadam zdolność do kreatywnej pracy nad nowymi funkcjonalnościami, ale również do efektywnej współpracy z kodem legacy. Doskonale znam system kontroli wersji git, korzystając z niego praktycznie od początku mojej kariery zawodowej. Moje doświadczenie obejmuje szeroki zakres języków programowania, praktyki pracy z CD/CI (m.in. GitLab, webpack, tworzenie pipelinów), operowanie narzędziem Docker, pisanie testów jednostkowych i automatycznych, oraz analizowanie dokumentacji i szkiców, np. w programie Figma.</p>
+          <p>Jestem otwarty na rozwój umiejętności miękkich i poznawanie nowych technologii i bibliotek. Staram się mieć dobre stosunki z zespołem i dbać o team spirit. Lubię współpracować i wymieniać się wiedzą.</p>
         </section>
         <section className="mb-8">
           <h2 className="text-2xl font-bold">Projekty</h2>
@@ -26,7 +30,7 @@ const Home = () => {
         <section>
           <h2 className="text-2xl font-bold">Kontakt</h2>
           <p className="mt-2">
-            Masz pytania lub chcesz się skontaktować? Skorzystaj z formularza kontaktowego lub odwiedź moje profile na mediach społecznościowych.
+            Masz pytania lub chcesz się skontaktować? Skorzystaj z formularza kontaktowego lub odwiedź moje profile na mediach społecznościowych. <a className='social' target="_blank" rel="noopener noreferrer" href={'https://www.linkedin.com/in/artur-szwagrzak-744431102/'}><FaLinkedin size={28} /></a>
           </p>
         </section>
       </main>
