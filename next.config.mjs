cfd0d8d90d90e61ca79401723cac435cfc63ea5a
Artur Szwagrzak <artur@szwagrzak.pl>
2024-08-19 22:27:27 +0200

    fix problems with experimental func webpack - entry option layer & top level await

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,13 @@ const nextConfig = {
         DATABASE_URL: process.env.DATABASE_URL,
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
       },
+      webpack: (config) => {
+        config.experiments = {
+            topLevelAwait: true,
+            layers: true
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
