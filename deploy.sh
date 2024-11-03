e14a588332e98f55dc457fff8d48555fb62148fd
Artur Szwagrzak <artur@szwagrzak.pl>
2024-11-03 22:38:10 +0100

    fix deploy github file

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,6 @@ USER='arturszwagrzak'
 HOST='arturszwagrzak.atthost24.pl'
 REMOTE_DIR='/home/arturszwagrzak/websites/szwagrzak_pl/'
 
-# Budowanie aplikacji
-npm install
-
-# Budowanie CSS
-npm run build:css
-
-# Budowanie aplikacji React
-npm run build
-
 # Przesyłanie plików na serwer
 rsync -avz --delete build/ $USER@$HOST:$REMOTE_DIR
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './app/components/Home';
 
+
 function App() {
+  console.log("App!");
+  console.log(Home);
   return (
     <Router>
         <Routes>
