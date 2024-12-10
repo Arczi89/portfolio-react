19d35a45ebf59661e11e63b6dc85eb81f185985e
Artur Szwagrzak <artur@szwagrzak.pl>
2024-12-10 16:04:20 +0100

    refactor duplicate items

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,31 +5,24 @@ import { Link, useLocation } from "react-router-dom";
 
 const Header: React.FC = () => {
   const location = useLocation();
+  const locationPaths = [
+    { url: "/", text: "Profil"},
+    { url: "/contact", text: "Kontakt"},
+    { url: "/interests", text: "Zainteresowania"},
+  ];
   return (
     <header className={`${styles.header}`}>
       <Logo />
       <nav>
-        <button
-          className={`btn btn--nav ${location.pathname === "/" ? "active" : ""}`}
-        >
-          <Link to="/" className={styles.navLink}>
-            Profil
-          </Link>
-        </button>
-        <button
-          className={`btn btn--nav ${location.pathname === "/contact" ? "active" : ""}`}
-        >
-          <Link to="/contact" className={styles.navLink}>
-            Kontakt
-          </Link>
-        </button>
-        <button
-          className={`btn btn--nav ${location.pathname === "/interests" ? "active" : ""}`}
-        >
-          <Link to="/interests" className={styles.navLink}>
-            Zainteresowania
-          </Link>
-        </button>
+        {locationPaths.map(path => {
+          return (<button key={path.url}
+            className={`btn btn--nav ${location.pathname === path.url ? "active" : ""}`}
+          >
+            <Link to={path.url} className={styles.navLink}>
+              {path.text}
+            </Link>
+          </button>)
+        })}
       </nav>
     </header>
   );
